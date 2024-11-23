ad3ac2557f115bebda730c429d1f7b9a33a1fdc2
Henry Zhang <henry.alakazhang@gmail.com>
2024-11-23 22:01:35 +1100

    Improve flavour text hint for puzz 5

--- src/app/pages/puzzles/5-parallel-universes.component.ts
+++ src/app/pages/puzzles/5-parallel-universes.component.ts
@@ -5,7 +5,7 @@ import { PuzzleComponent } from '../../ui/puzzle.component';
   template: `<app-puzzle
     puzzleId="5"
     titleName="Parallel Universes"
-    flavour="You've finally arrived at your seaside holiday destination! Except... something's wrong here. Was that the right flight? Or did you take a bad turn somewhere?"
+    flavour="You've finally arrived at your seaside holiday destination in that beautiful Commonwealth nation! Except... something's wrong here. Was that the right flight? Or did you take a bad turn somewhere?"
   >
     <div class="flex flex-col max-w-3xl">
       @for (img of [1, 2, 3, 4, 5, 6]; track $index) {
