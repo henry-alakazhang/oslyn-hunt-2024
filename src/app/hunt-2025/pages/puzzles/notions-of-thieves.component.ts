32deaaec35349acd421d3985c2560d51b24fa7c6
Henry Zhang <henry.alakazhang@gmail.com>
2025-06-22 22:05:07 +1000

    Add meta

--- src/app/hunt-2025/pages/puzzles/notions-of-thieves.component.ts
+++ src/app/hunt-2025/pages/puzzles/notions-of-thieves.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { PuzzleComponent } from '../../../ui/puzzle.component';
 
 @Component({
   selector: 'app-notions-of-thieves',
-  template: ``,
+  template: `
+    <app-puzzle
+      puzzleId="2025-m"
+      titleName="Notions of Thieves"
+      flavour=""
+      [cannedHints]="[]"
+    >
+      <img src="./2025/notions-of-thieves.jpg" />
+    </app-puzzle>
+  `,
+  imports: [PuzzleComponent],
 })
 export class NotionsOfThievesComponent {}
