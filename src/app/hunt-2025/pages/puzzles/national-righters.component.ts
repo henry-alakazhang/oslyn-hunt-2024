4549d439814fef109dce3d59c69efc9ecc544db6
Henry Zhang <henry.alakazhang@gmail.com>
2025-06-28 01:10:14 +1000

    Change presentation of National Righters

--- src/app/hunt-2025/pages/puzzles/national-righters.component.ts
+++ src/app/hunt-2025/pages/puzzles/national-righters.component.ts
@@ -11,17 +11,141 @@ import { PuzzleComponent } from '../../../ui/puzzle.component';
       answerHash=""
       [cannedHints]="[]"
     >
-      <p>Common MTG limited format (5)</p>
-      <p> Trouble or afflict someone (3)</p> <p> Give up territory (4)</p>
-      <p> An ugly duckling (6)</p> <p> Don't fall for it! (4)</p>
-      <p> A spoken exam (4)</p> <p> Kind and friendly (4)</p>
-      <p> Miku's favourite vegetable (4)</p>
-      <p> A heiroglyphic or an emoji, for example (6)</p>
-      <p> Used to shred cheese (6)</p>
-      <p> Whipped dessert with air bubbles (6)</p>
-      <p> Has a disparity with casters, in D&D parlance (7)</p>
-      <p> Cuisine featuring tom yum and massaman (4)</p>
-      <p> Measure of energy (5)</p> <p> Devoid of life (6)</p>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/1.mp3"
+          alt="Common MTG limited format"
+          controls
+        ></audio>
+        (5)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/2.mp3"
+          alt="Trouble or afflict someone"
+          controls
+        ></audio>
+        (3)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/3.mp3"
+          alt="Give up territory"
+          controls
+        ></audio>
+        (4)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/4.mp3"
+          alt="An ugly duckling"
+          controls
+        ></audio>
+        (6)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/5.mp3"
+          alt="Don't fall for it"
+          controls
+        ></audio>
+        (4)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/6.mp3"
+          alt="A spoken exam"
+          controls
+        ></audio>
+        (4)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/7.mp3"
+          alt="Kind and friendly"
+          controls
+        ></audio>
+        (4)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/8.mp3"
+          alt="Miku's favourite vegetable"
+          controls
+        ></audio>
+        (4)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/9.mp3"
+          alt="A heiroglyphic or emoji, for example"
+          controls
+        ></audio>
+        (6)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/10.mp3"
+          alt="Used to shred cheese"
+          controls
+        ></audio>
+        (6)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/11.mp3"
+          alt="Whipped dessert with air bubbles"
+          controls
+        ></audio>
+        (6)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/12.mp3"
+          alt="Has a disparity with casters, in D&D parlance"
+          controls
+        ></audio>
+        (7)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/13.mp3"
+          alt="Cuisine featuring tom yum"
+          controls
+        ></audio>
+        (4)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/14.mp3"
+          alt="Measure of energy"
+          controls
+        ></audio>
+        (5)
+      </div>
+      <div class="flex flex-row items-center mb-2">
+        <audio
+          class="mr-2"
+          src="./2025/national-righters/15.mp3"
+          alt="Devoid of life"
+          controls
+        ></audio>
+        (6)
+      </div>
     </app-puzzle>
   `,
   imports: [PuzzleComponent],
